1da1ab73f2ac85a234bcd9c65b183d94459b2ba4
vee anti <113473346+veeeanti@users.noreply.github.com>
2025-06-20 18:31:49 -0500

    Update Template Make Desktop Shortcut Bat.bat

--- Template Make Desktop Shortcut Bat.bat
+++ Template Make Desktop Shortcut Bat.bat
@@ -8,6 +8,7 @@ echo sLinkFile = "%USERPROFILE%\Desktop\[Game Name Here].lnk" >> %SCRIPT%
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
 echo oLink.TargetPath = "%cd%\[Game Exe Name Here].exe" >> %SCRIPT%
 echo oLink.WorkingDirectory = "%cd%" >> %SCRIPT%
+:: Uncomment this next line if the game requires launch arguments.
 :: echo oLink.Arguments = "" >> %SCRIPT%
 echo oLink.IconLocation =  "%cd%\icon.ico" >> %SCRIPT%
 echo oLink.Save >> %SCRIPT%
