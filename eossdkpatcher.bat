44ffedc54d569c40ddeacd4ff4a4e82f5bfd997d
vee anti <113473346+veeeanti@users.noreply.github.com>
2025-06-29 02:15:41 -0500

    Update eossdkpatcher.bat
    tidied up script

--- eossdkpatcher.bat
+++ eossdkpatcher.bat
@@ -12,25 +12,24 @@ set "origEOSdll64=%~dp1\EOSSDK-Win64-Shipping_o.dll"
 set "urlfile=%~dp0\UnionCrax.url"
 set "nfo=%~dp0\blankUNION.nfo"
 
-if exist %targetEOSdll32% goto:32
 if exist %origEOSdll32% goto:32o
 if exist %origEOSdll64% goto:64o
+if exist %targetEOSdll32% goto:32
+if exist %targetEOSdll64% goto:64
 
 :32
 ren "%targetEOSdll32%" EOSSDK-Win32-Shipping_o.dll
 copy /Y "%emuEOSdll32%" %targetEOSdll32%
 if exist %targetEOSdll64% goto:64
+goto:extrastuff
 
 :64
 ren "%targetEOSdll64%" EOSSDK-Win64-Shipping_o.dll
 copy /Y "%emuEOSdll64%" %targetEOSdll64%
-goto:url
+goto:extrastuff
 
-:url
+:extrastuff
 copy /Y "%urlfile%" "%~dp1"
-goto:nfo
-
-:nfo
 copy /Y "%nfo%" "%~dp1\UNION.nfo"
 endlocal
 exit
